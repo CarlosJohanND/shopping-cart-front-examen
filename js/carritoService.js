84546555c641219b55e88557857f44a048df76cc
cristiancrakl <moralescadenac@gmail.com>
2025-06-04 20:38:48 -0500

    casi terminando funcionalidad del carrito

--- js/carritoService.js
+++ js/carritoService.js
@@ -1,6 +1,6 @@
 function getCarrito() {
   document.getElementById("cardHeader").innerHTML =
-    '<h4><i class="fa-solid fa-users"></i> Carrito de compras</h4>';
+    '<h4><i class="fa-solid fa-Carts"></i> Carrito de compras</h4>';
   fetch("https://fakestoreapi.com/carts", {
     method: "GET",
     headers: {
@@ -20,13 +20,13 @@ function getCarrito() {
       if (response.status === 200) {
         let listCart = `
             <button type="button" class="btn btn-outline-success" onclick="addCart()">
-                <i class="fa-solid fa-user-plus"></i>
+                <i class="fa-solid fa-Cart-plus"></i>
             </button>
                 <table class="table">
                     <thead>
                         <tr>
                         <th scope="col">ID</th>
-                        <th scope="col">CartId</th>
+                        <th scope="col">Date</th>
                         <th scope="col">More info</th>
                         </tr>
                     </thead>
@@ -35,8 +35,8 @@ function getCarrito() {
         response.body.forEach((cart) => {
           listCart = listCart.concat(`
                     <tr>
-                        <td>${cart.id}</td>
                         <td>${cart.userId}</td>
+                        <td>${cart.date}</td>
                         
                         <td><button type="button" class="btn btn-outline-info" onclick="showInfoCart('${cart.id}')"><i class="fa-solid fa-eye"></i></button>
                         </td>
@@ -53,4 +53,55 @@ function getCarrito() {
           "<h3>No se encontraron usuarios</h3>";
       }
     });
+}
+
+function showInfoCart(cartId) {
+  fetch("https://fakestoreapi.com/carts/" + cartId, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((result) => {
+      return result.json().then((data) => {
+        return {
+          status: result.status,
+          body: data,
+        };
+      });
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        showModalCart(response.body);
+      } else {
+        document.getElementById("info").innerHTML =
+          "<h3>No se encontro el Producto</h3>";
+      }
+    });
+}
+
+function showModalCart(cart) {
+  const modalCart = `
+        <!-- Modal -->
+        <div class="modal fade" id="modalCart" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel"> Show Cart</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                        <h5 class="card-title">cart info</h5>
+                        <p class="card-text">IDcarrito : ${cart.userId}</p>
+                        <p class="card-text">Fecha : ${cart.date}</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    `;
+  document.getElementById("showModal").innerHTML = modalCart;
+  const modal = new bootstrap.Modal(document.getElementById("modalCart"));
+  modal.show();
 }
